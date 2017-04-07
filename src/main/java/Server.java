da856780f416d367f5227ae0115c1c135a7fff0d
和田真理菜 <marina.wada@topgate.co.jp>
2017-04-07 18:14:18 +0900

    Testの方にテンプレート移動

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -11,60 +11,16 @@ import java.net.Socket;
  */
 public class Server {
 
-    private static final int PORT = 8080;
+    //private static final int PORT = 8080;
+    private static int PORT= 8080;
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("start up http server...");
+    public int getPort(int portnum) {
+        this.PORT = portnum;
+        return PORT;
+    }
 
-        ServerSocket serverSocket = null;
-        Socket socket = null;
-        PrintWriter writer = null;
-        BufferedReader br = null;
 
-        try {
-            serverSocket = new ServerSocket(PORT);
+    public static void main(String[] args){
 
-            while (true) {
-                socket = serverSocket.accept();
-                System.out.println("request incoming...");
-
-                // リクエスト
-                br = new BufferedReader(new InputStreamReader(
-                        socket.getInputStream()));
-                String str;
-                while (!(str = br.readLine()).equals("")) {
-                    System.out.println(str);
-                }
-
-                // レスポンス
-                writer = new PrintWriter(socket.getOutputStream(), true);
-                StringBuilder builder = new StringBuilder();
-
-                builder.append("HTTP/1.1 200 OK").append("\n");
-                builder.append("Content-Type: text/html").append("\n");
-                builder.append("\n");
-                builder.append("<html><head><title>Hello world!</title></head><body><h1>Hello world!</h1>Hi!</body></html>");
-
-                System.out.println("responce...");
-                System.out.println(builder.toString() + "\n");
-                writer.println(builder.toString());
-
-                socket.close();
-            }
-
-        } finally {
-            if (serverSocket != null) {
-                serverSocket.close();
-            }
-            if (socket != null) {
-                socket.close();
-            }
-            if (writer != null) {
-                writer.close();
-            }
-            if (br != null) {
-                br.close();
-            }
-        }
     }
 }
--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -10,6 +10,61 @@ import java.net.Socket;
  *
  */
 public class ServerTest {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        Server server = new Server();
+        System.out.println("start up http server...");
+
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        PrintWriter writer = null;
+        BufferedReader br = null;
+
+        try {
+            serverSocket = new ServerSocket(server.getPort(8080));
+
+
+            while (true) {
+                socket = serverSocket.accept();
+                System.out.println("request incoming...");
+
+                // リクエスト
+                br = new BufferedReader(new InputStreamReader(
+                        socket.getInputStream()));
+                String str;
+                while (!(str = br.readLine()).equals("")) {
+                    System.out.println(str);
+                }
+
+                // レスポンス
+                writer = new PrintWriter(socket.getOutputStream(), true);
+                StringBuilder builder = new StringBuilder();
+
+                builder.append("HTTP/1.1 200 OK").append("\n");
+                builder.append("Content-Type: text/html").append("\n");
+                builder.append("\n");
+                builder.append("<html><head><title>Hello world!</title></head><body><h1>Hello world!</h1>Hi!</body></html>");
+
+                System.out.println("responce...");
+                System.out.println(builder.toString() + "\n");
+                writer.println(builder.toString());
+
+                socket.close();
+            }
+
+        }finally {
+
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+            if (writer != null) {
+                writer.close();
+            }
+            if (br != null) {
+                br.close();
+            }
+        }
     }
 }
