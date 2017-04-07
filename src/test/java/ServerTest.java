015ee681e580f13bb2667057d1fe151b38326473
和田真理菜 <marina.wada@topgate.co.jp>
2017-04-07 17:20:06 +0900

    Testとブランチ作成の確認

---
+++ src/test/java/ServerTest.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 簡易HTTPサーバ
+ *
+ */
+public class ServerTest {
+    public static void main(String[] args){
+    }
+}
