2954164f3dd504c0c15f6ce5b8e9273165acaa84
和田真理菜 <marina.wada@topgate.co.jp>
2017-04-20 18:26:52 +0900

    Responseで改行コードが間違っていたのを修正

--- src/main/java/jp/co/topgate/atoze/web/ServerHandler.java
+++ src/main/java/jp/co/topgate/atoze/web/ServerHandler.java
@@ -13,23 +13,23 @@ class ServerHandler {
     private Status status = new Status();
 
     public void handle(InputStream in, OutputStream out, int PORT) throws IOException {
-        request.readRequestText(in, this.hostname + ":" + PORT);
-        System.out.println(request.getHeaderText());
+        this.request.readRequestText(in, this.hostname + ":" + PORT);
+        System.out.println(this.request.getHeaderText());
 
-        String host = request.getRequestValue("Host");
-        if (host == null || request.getMethod() == null || !host.startsWith(this.hostname + ":" + PORT)) {
+        String host = this.request.getRequestValue("Host");
+        if (host == null || this.request.getMethod() == null || !host.startsWith(this.hostname + ":" + PORT)) {
             setError(400);
-            response.writeTo(out, this.status);
+            this.response.writeTo(out, this.status);
             return;
         }
 
-        if (!request.getMethod().equals("GET")) {
+        if (!this.request.getMethod().equals("GET")) {
             setError(405);
-            response.writeTo(out, this.status);
+            this.response.writeTo(out, this.status);
             return;
         }
 
-        String filePath = "." + request.getFilePath();
+        String filePath = "." + this.request.getFilePath();
 
         if (filePath.endsWith("/")) {
             filePath += "index.html";
@@ -39,32 +39,32 @@ class ServerHandler {
 
         if(!file.exists() || !file.isFile()){
             setError(404);
-            response.writeTo(out, this.status);
+            this.response.writeTo(out, this.status);
             return;
         }
 
         if(!file.canRead()){
             setError(403);
-            response.writeTo(out, this.status);
+            this.response.writeTo(out, this.status);
             return;
         }
 
         this.status.setStatus(200);
-        response.addText("Content-Type", ContentTypeUtil.getContentType(filePath));
-        response.setResponseBody(file);
-        response.writeTo(out, this.status);
+        this.response.addText("Content-Type", ContentTypeUtil.getContentType(filePath));
+        this.response.setResponseBody(file);
+        this.response.writeTo(out, this.status);
 
-        System.out.println(response.getResponse());
+        System.out.println(this.response.getResponse());
 
     }
 
     private void setError(int error) {
-        this.status.setStatus(error);
+        status.setStatus(error);
         File file = new File(error + ".html");
         if (file.exists() && file.isFile() && file.canRead()) {
-            response.setResponseBody(file);
+            this.response.setResponseBody(file);
         } else {
-            response.setResponseBody("<html><head><title>" + status.getStatus() + "</title></head><body><h1>" +
+            this.response.setResponseBody("<html><head><title>" + status.getStatus() + "</title></head><body><h1>" +
                     status.getStatus() + "</h1></body></html>");
         }
     }
--- src/test/java/jp/co/topgate/atoze/web/HTTPResponseTest.java
+++ src/test/java/jp/co/topgate/atoze/web/HTTPResponseTest.java
@@ -25,7 +25,7 @@ public class HTTPResponseTest {
         status.setStatus(200);
         response.addText("ContentType", "text/html");
         response.writeTo(output, status);
-        assertThat("HTTP/1.1 "+status.getStatus()+"\\n"+"ContentType: text/html\\n", is(response.getResponse()));
+        assertThat("HTTP/1.1 "+status.getStatus()+"\n"+"ContentType: text/html\n", is(response.getResponse()));
 
     }
 }
